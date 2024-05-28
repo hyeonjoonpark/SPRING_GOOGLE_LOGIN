68fb5aa598e1241e85aeadb5297e80ea5282947a
hyeonjoonpark <pjjoon1379@gmail.com>
2024-05-28 15:47:05 +0900

    SET: project setting

---
+++ src/main/java/spring/security/oauth/GoogleOauthApplication.java
@@ -0,0 +1,13 @@
+package spring.security.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleOauthApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GoogleOauthApplication.class, args);
+  }
+
+}
---
+++ src/test/java/spring/security/oauth/GoogleOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.security.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleOauthApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
